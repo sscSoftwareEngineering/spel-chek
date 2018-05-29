6f8557849066afb35945608c4aa775c17300258a
sscSoftwareEngineering <39412464+sscSoftwareEngineering@users.noreply.github.com>
2018-05-29 16:57:42 -0400

    Use user input for files instead of file arguments

--- SpelCheckCLI.java
+++ SpelCheckCLI.java
@@ -6,25 +6,38 @@
 package spelChek;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class SpelCheckCLI {
     public static void main(String[] args) {
         final String PATH = System.getProperty("user.dir") + '\\';
-        String dataFile = args[0];
-        String dictFile = args[1];
-        ArrayList<String> in = new ArrayList<>();
+        ArrayList<String> input = new ArrayList<>();
         ArrayList<String> dict = new ArrayList<>();
         ArrayList<String> unknown;
+        Scanner in = new Scanner(System.in);
+        
+        System.out.println("Welcome to SpelChek.");
+        System.out.println("The current working directory is:");
+        System.out.println(PATH);
+        System.out.println("Please enter the file name or full path of the text "
+                + "file you would like to check");
+        
+        String dataFile = in.nextLine();
+        
+        System.out.println("Please enter the file name or full path of the "
+                + "dictionary");
+        
+        String dictFile = in.nextLine();
         
         //Use DataAccess to load data into ArrayLists
         DataAccess da = new DataAccess();
         try {
-            in = da.loadData(PATH, dataFile);
+            input = da.loadData(PATH, dataFile);
             dict = da.loadData(PATH, dictFile);
             
             //Spell check the data using the dictionary
             SpelChek spellChecker = new SpelChek();
-            unknown = spellChecker.getUnknown(in, dict);
+            unknown = spellChecker.getUnknown(input, dict);
 
             for (String item : unknown) {
                 System.out.println(item + " is an unknown word");
